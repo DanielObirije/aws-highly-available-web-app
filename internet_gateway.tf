cd46351bff12194eca400a14dbf1f83bb39e8841
DanielObirije <obirijedaniel@gmail.com>
2026-09-03 19:50:26 +0100

    added lb and target group

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -4,3 +4,30 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.project_name}-igw"
   }
 }
+
+
+resource "aws_lb" "main" {
+    name =  "${var.project_name}-lb"
+    internal = false
+    load_balancer_type = "application"
+    
+    security_groups =[ aws_security_group.web.id]
+    subnets = aws_subnet.public_subnets[*].id
+
+   tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+   name =  "${var.project_name}-tg"
+   port = 80
+   protocol = "HTTP"
+   vpc_id = aws_vpc.main.id
+
+   health_check {
+     enabled = true
+     
+   }
+
+}
--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnets" {
   count = 3
   vpc_id = aws_vpc.main.id
   cidr_block = [
@@ -22,4 +22,51 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+   tags ={
+    Name = "${var.project_name}-rt"
+  }
+}
 
+resource "aws_route_table_association" "public" {
+ count = 3
+ subnet_id = aws_subnet.public_subnets[count.index].id
+ route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "web" {
+  name =  "${var.project_name}-web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id = aws_vpc.main.id
+
+  ingress  {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress{
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   tags ={
+    Name = "${var.project_name}-web-sg"
+  }
+}
